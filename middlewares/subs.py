06c1f03c8335fd42cbcadcc33f72a0e8d40fea0d
madaminovabdulaziz <105636505+madaminovabdulaziz@users.noreply.github.com>
2023-02-03 15:07:55 +0500

    Update subs.py

--- middlewares/subs.py
+++ middlewares/subs.py
@@ -2,9 +2,8 @@ import logging
 from aiogram import types
 from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
-from states.main_state import Main
+from keyboards.inline.subs import check
 from data.config import CHANNELS
-from keyboards.inline.subs import the_btn
 from utils.misc import subscription
 from loader import bot
 
@@ -22,17 +21,17 @@ class BigBrother(BaseMiddleware):
         else:
             return
 
-        result = "Botdan foydalanish uchun quyidagi kanallarga obuna bo'ling:\n"
-
+        result = "Botdan foydalanish uchun quyidagi kanalga <b>obuna</b> bo'ling:\n"
+        final_status = True
         for channel in CHANNELS:
             status = await subscription.check(user_id=user,
                                               channel=channel)
-
+            final_status *= status
             channel = await bot.get_chat(channel)
             if not status:
                 invite_link = await channel.export_invite_link()
                 result += (f"👉 <a href='{invite_link}'>{channel.title}</a>\n")
 
-            else:
-                await update.message.answer(result, disable_web_page_preview=True)
-                raise CancelHandler()
+        if not final_status:
+            await update.message.answer(result, reply_markup=check, disable_web_page_preview=True)
+            raise CancelHandler()
