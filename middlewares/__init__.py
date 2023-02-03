ac0e214cea97aa2fbf56e9e27d91c1e9a73eac4e
madaminovabdulaziz <105636505+madaminovabdulaziz@users.noreply.github.com>
2023-02-03 15:08:23 +0500

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -2,9 +2,9 @@ from aiogram import Dispatcher
 
 from loader import dp
 from .throttling import ThrottlingMiddleware
-#from .subs import BigBrother
+from .subs import BigBrother
 
 
 if __name__ == "middlewares":
     dp.middleware.setup(ThrottlingMiddleware())
-    #dp.middleware.setup(BigBrother())
+    dp.middleware.setup(BigBrother())
