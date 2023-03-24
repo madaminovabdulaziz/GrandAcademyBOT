b71ca08e512a9674e7f3fc54cd47e81aa57fa29a
Abdulaziz Madaminov <105636505+madaminovabdulaziz@users.noreply.github.com>
2023-03-24 11:26:26 +0500

    added rating for usual users

--- handlers/users/ratingUsers.py
+++ handlers/users/ratingUsers.py
@@ -7,7 +7,7 @@ from states.main_state import Main
 
 @dp.message_handler(text="📊Reyting ko'rish", state=Main.main_menu)
 async def showRatingToUsers(message: Message, state: FSMContext):
-    await message.answer("Iltimos, test ID-ni kiriting\n\n⚠️<b>Diqqat!</b>\nReyting faqat:\n-Prezident maktab o'quvchilari\n-Abiturientlar uchun ko'rsatiladi!")
+    await message.answer("Iltimos, test ID-ni kiriting!")
     await state.set_state("getTESTIDUser")
 
 home_btn = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -19,23 +19,17 @@ async def showF(message: Message, state: FSMContext):
     test_id = message.text
     test_bor = await db.get_test_by_id(test_id)
     if test_bor:
-        test_turi = await db.get_test_type(test_id)
-        if test_turi == "specialPM" or test_turi == "specialA":
-            subject = await db.get_test_name(test_id)
-            rating = await db.show_rating_by_user(test_id)
-            show_rating = f"📕 Fan: <b>{subject}</b>\n🎛 Test kodi: <b>{test_id}</b>\n\n{test_id}-kodli test bo'yicha REYTING⬇️\n\n"
-            i = 1
-            for user in rating:
-                show_rating += "{0}) {1} - {2} ball\n".format(i, user['full_name'], user['ball'])
-                i += 1
+        subject = await db.get_test_name(test_id)
+        rating = await db.show_rating_by_user(test_id)
+        show_rating = f"📕 Fan: <b>{subject}</b>\n🎛 Test kodi: <b>{test_id}</b>\n\n{test_id}-kodli test bo'yicha REYTING⬇️\n\n"
+        i = 1
+        for user in rating:
+            show_rating += "{0}) {1} - {2} ball\n".format(i, user['full_name'], user['ball'])
+            i += 1
 
-            await message.answer(show_rating)
-            await message.answer("🏡 Bosh menyu", reply_markup=main_menu)
-            await Main.main_menu.set()
-        else:
-            await message.answer("Bu test block test emas!")
-            await message.answer("Qayta kiriting!", reply_markup=ReplyKeyboardRemove())
-            return
+        await message.answer(show_rating)
+        await message.answer("🏡 Bosh menyu", reply_markup=main_menu)
+        await Main.main_menu.set()
 
     else:
         await message.answer("Afsuski bu test bazadan topilmadi, qayta kiriting!", reply_markup=ReplyKeyboardRemove())
