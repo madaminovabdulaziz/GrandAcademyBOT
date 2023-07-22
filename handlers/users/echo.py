cf5e6a593ed3b01d2e392a6a345d5b9c098c7fb3
Abdulaziz Madaminov <105636505+madaminovabdulaziz@users.noreply.github.com>
2023-07-22 22:55:34 +0500

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,29 @@
 from aiogram import types
-
-from loader import dp
+import textwrap
+from loader import dp, db
+from data.config import ADMINS
 
 
 # Echo bot
-@dp.message_handler(state=None)
+@dp.message_handler(commands=['users'], state="*", chat_id=ADMINS)
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+
+
+    users = await db.get_all_users()
+
+    i = 1
+    data = "<b>BARCHA FOYDALUVCHILAR\n\n\n</b>"
+    max_message_length = 4096  # Adjust this value based on the maximum message length allowed by your messaging platform
+
+    for user in users:
+        data += "{0}) {1} - {2} - {3} - {4} - {5}\n".format(i, user['id'], user['full_name'], user['username'],
+                                                            user['telegram_id'], user['phone'])
+        i += 1
+
+    # Split the data into smaller chunks using textwrap
+    data_chunks = textwrap.wrap(data, width=max_message_length, replace_whitespace=False)
+
+    # Send each chunk as a separate message
+    for chunk in data_chunks:
+        await message.answer(chunk)
+
